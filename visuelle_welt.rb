c02f69ee2d99470e336f30f713ff6cb33788b115
Benjamin Maier <benjaminfrankmaier@gmail.com>
2018-04-30 00:23:39 +0200

    updated version

--- visuelle_welt.rb
+++ visuelle_welt.rb
@@ -1,23 +1,26 @@
 # Welcome to Sonic Pi v2.11.1
 
-chrd_cutoff = 80
-hh_vol = 0.5
-bd_vol = 0.5
-rnd_high_vol = 0.2
-pent_vol = 0.5
-rnd_low_vol = 1
-chrd_vol = 1
-bass_vol = 1
-piano_vol = 0.3
-salvation_vol = 0.8
+chrd_cutoff = 120
+hh_total = 0
+hh_vol = hh_total * 1*1
+hh_open_vol = hh_total * 1*1
+bd_vol = 0.5*1.5*0
+rnd_high_vol = 0.4*1
+pent_vol = 1*0
+rnd_low_vol = 0
+chrd_vol = 0
+bass_vol = 2*0
+piano_vol = 0.5*0
+salvation_vol = 1.5*0
+welt_vol = 1
 
 high_synth_id = 1
 high_synth = [:beep, :tri, :chipbass, :pretty_bell][high_synth_id]
 
-is_break = false
-slice_break = false
+salvation_break = true
+slice_voice = true
 
-rnd_high_release_rand_max = 0.3
+rnd_high_release_rand_max = 0.5
 
 use_bpm 120
 
@@ -34,14 +37,18 @@ end
 live_loop :rnd_high do
   cue :bd
   use_synth high_synth
-  with_fx :reverb, mix: 0.5, room: 1 do
-    with_fx :flanger, mix: 0.5, delay: 0.5 do
-      with_fx :echo, mix: 0.4, phase: 3.0/4.0 do
+  with_fx :reverb, mix: 1, room: 1 do
+    with_fx :flanger, mix: 0.4, delay: 0.5 do
+      with_fx :echo, mix: 0.5, phase: 3.0/4.0, decay: 10 do
         8.times do
-          #play chord(:E4, :major).choose, release: rrand(0,rnd_high_release_rand_max), amp: 1*rnd_high_vol, attack: 0.01
-          #play chord(:Cs4, :minor).choose, release: rrand(0,rnd_high_release_rand_max), amp: 1*rnd_high_vol, attack: 0.01
-          play chord(:Gs4, :minor).choose, release: rrand(0,rnd_high_release_rand_max), amp: 1*rnd_high_vol, attack: 0.01
-          sleep 1.0/3.0
+          if rnd_high_vol>0
+            play chord(:E4, :major).choose, release: rrand(0,rnd_high_release_rand_max), amp: 1*rnd_high_vol, attack: 0.01
+            play chord(:Cs5, :minor).choose, release: rrand(0,rnd_high_release_rand_max), amp: 1*rnd_high_vol, attack: 0.01
+            play chord(:Gs4, :minor).choose, release: rrand(0,rnd_high_release_rand_max), amp: 1*rnd_high_vol, attack: 0.01
+          end
+          #sleep 1.0/3.0
+          #sleep [0.25, 0.125, 1.0/3.0].choose
+          sleep [1, 2,3.0].choose
         end
       end
     end
@@ -95,23 +102,27 @@ with_fx :panslicer, mix: 0.3, rate: 1 do
 end
 
 
-with_fx :lpf, mix: 1, cutoff: 100 do
-  live_loop :bd do
-    sample :bd_tek, amp: 5*bd_vol
-    sleep 1
-  end
+#with_fx :lpf, mix: 1, cutoff: 120 do
+live_loop :bd do
+  sample :bd_klub, amp: 5*bd_vol
+  sleep 1
 end
+#end
 
-live_loop :hh do
-  cue :bd
-  sample :drum_cymbal_closed, amp: 1.0*hh_vol
-  sleep 1/4.0
-  sample :drum_cymbal_closed, amp: 0.8*hh_vol
-  sleep 1/4.0
-  sample :drum_cymbal_open, amp: 0.2*hh_vol
-  sleep 1/4.0
-  sample :drum_cymbal_closed, amp: 0.8*hh_vol
-  sleep 1/4.0
+with_fx :reverb, room: 0.1, mix: 0.5 do
+  live_loop :hh do
+    cue :bd
+    1.times do
+      sample :drum_cymbal_closed, amp: 0.7*hh_vol*rrand(0.9,1.1), attack: 0.01
+      sleep 1/4.0+rrand(-0.01,0.01)
+      sample :drum_cymbal_closed, amp: 0.8*hh_vol*rrand(0.9,1.1), attack: 0.01
+      sleep 1/4.0+rrand(-0.01,0.01)
+      sample :drum_cymbal_open, amp: 0.25*hh_open_vol*rrand(0.9,1.1), attack: 0.01
+      sleep 1/4.0+rrand(-0.01,0.01)
+      sample :drum_cymbal_closed, amp: 0.8*hh_vol*rrand(0.9,1.1), attack: 0.01
+      sleep 1/4.0+rrand(-0.01,0.01)
+    end
+  end
 end
 
 live_loop :welt do
@@ -121,7 +132,7 @@ live_loop :welt do
       with_fx :panslicer, mix: 0.3, rate: 4 do
         with_fx :hpf, mix: 1, cutoff: 80 do
           sleep 64
-          sample "/Users/bfmaier/Music/sonic_pi/diese_visuelle_welt/visuelle_welt.wav", amp: 4
+          sample "/Users/bfmaier/Music/sonic_pi/diese_visuelle_welt/visuelle_welt.wav", amp: 4*welt_vol
           sleep 64
         end
       end
@@ -144,11 +155,18 @@ with_fx :echo, mix: 0.8, phase: 3.0/4.0,  decay: 10 do
     #slice_idx = rand_i(16)
     #sample "/Users/bfmaier/Music/sonic_pi/diese_visuelle_welt/my_salvation.wav", slice: slice_idx
     # sleep 12
-    if not is_break
-      sample "/Users/bfmaier/Music/sonic_pi/diese_visuelle_welt/my_salvation.wav", rate: -1, slice: rand_i(4), num_slices: 4, amp: 1.0*salvation_vol
+    if not salvation_break
+      if slice_voice
+        sample "/Users/bfmaier/Music/sonic_pi/diese_visuelle_welt/my_salvation.wav", rate: -1, slice: rand_i(4), num_slices: 4, amp: 1.0*salvation_vol
+      else
+        with_fx :hpf, mix: 1, cutoff: 80 do
+          sample "/Users/bfmaier/Music/sonic_pi/diese_visuelle_welt/my_salvation.wav", rate: 1, amp: 1.0*salvation_vol
+        end
+        sleep 8
+      end
     else
       with_fx :hpf, mix: 1, cutoff: 80 do
-        if slice_break
+        if slice_voice
           sample "/Users/bfmaier/Music/sonic_pi/diese_visuelle_welt/ouh.wav", rate: -1, slice: rand_i(5), num_slices: 5, amp: 1.0*salvation_vol
         else
           sample "/Users/bfmaier/Music/sonic_pi/diese_visuelle_welt/ouh.wav", rate: 1, amp: 1.0*salvation_vol
